3345afdb59a0a8ea0832d784c0a5ce8ed255fbb6
Nafees10 <nafeeshassan12345@gmail.com>
2017-06-01 18:04:25 +0500

    Added some more functions to `utils.misc`

--- source/utils/misc.d
+++ source/utils/misc.d
@@ -4,6 +4,7 @@
 module utils.misc;
 
 import std.stdio;
+import std.datetime;
 
 //These names are easier to understand
 ///`integer is a `long` on 64 bit systems, and `int` on 32 bit systems
@@ -68,4 +69,66 @@ T[] insertArray(T)(T[] dat, T[] ins, uinteger pos){
 	ar1 = dat[0..pos];
 	ar2 = dat[pos..dat.length];
 	return ar1~ins~ar2;
+}
+
+/// Returns a random number between 0 and `max`, or zero, or `max`
+uinteger getRand(uinteger max){
+	SysTime currTime = Clock.currTime;
+	uinteger tm = currTime.second+(currTime.minute*60)+(currTime.hour*60*60);// stores time in seconds
+	uinteger r;
+	if (max>0){
+		if (tm>max){
+			r = tm % max;
+		}else{
+			r = tm;
+		}
+	}else{
+		r = 0;
+	}
+	return r;
+}
+
+/// returns the reverse of a string
+string reverse(string s){
+	integer i, writePos = 0;
+	char[] r;
+	r.length = s.length;
+
+	i = s.length-1;
+	do{
+		r[writePos] = s[i];
+		writePos ++;
+		i --;
+	}while(i > 0);
+	return cast(string)r;
+}
+
+/// Returns a string with all uppercase alphabets converted into lowercase
+string lowercase(string s){
+	string tmstr;
+	ubyte tmbt;
+	for (integer i=0;i<s.length;i++){
+		tmbt = cast(ubyte) s[i];
+		if (tmbt>=65 && tmbt<=90){
+			tmbt+=32;
+			tmstr ~= cast(char) tmbt;
+		}else{
+			tmstr ~= s[i];
+		}
+	}
+	
+	return tmstr;
+}
+
+/// returns true if all characters in a string are alphabets, uppercase, lowercase, or both
+bool isAlphabet(string s){
+	s = lowercase(s);
+	uinteger i;
+	bool r=true;
+	ubyte chCode;
+	for (i=0;i<s.length;i++){
+		chCode = cast(ubyte)s[i];
+		if (chCode<97 || chCode>122){r=false;break;}
+	}
+	return r;
 }
