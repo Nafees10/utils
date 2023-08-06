370ca2314b03e8c072c32e1447313eb31c630583
Nafees Hassan <nafeeshassan16@gmail.com>
2023-08-06 23:52:21 +0500

    Bug fix in radixSort, where it would not work for non-integer type arrays

--- source/utils/sort.d
+++ source/utils/sort.d
@@ -30,7 +30,7 @@ T[] partialSort(alias val = "a", T)(T[] input, ulong count){
 		selection = merge!val(selection, radixSort!val(temp[0 .. i]), count);
 	return selection;
 }
-/// 
+///
 unittest{
 	uint[] input = [7, 0, 5, 4, 6, 8, 9];
 	assert(partialSort(input, 1) == [0]);
@@ -42,7 +42,7 @@ unittest{
 /// Radix sort. ascending order
 T[] radixSort(alias val = "a", T)(T[] input){
 	alias valGet = unaryFun!val;
-	static immutable ubyte end = T.sizeof * 8;
+	enum ubyte end = typeof(valGet(input[0])).sizeof * 8;
 	size_t[256] counts;
 	T[] output = new T[input.length];
 	for (ubyte i = 0; i < end; i += 8){
@@ -57,7 +57,7 @@ T[] radixSort(alias val = "a", T)(T[] input){
 	}
 	return input;
 }
-/// 
+///
 unittest{
 	uint[] input = [7, 0, 5, 4, 6, 8, 9];
 	assert(radixSort(input) == [0, 4, 5, 6, 7, 8, 9]);
@@ -79,7 +79,7 @@ T[] mergeSort(alias val = "a", T)(T[] arr, ulong maxLen = 0){
 					mergeSort!val(arr[mid .. $], maxLen),
 					maxLen);
 }
-/// 
+///
 unittest{
 	uint[] input = [7, 0, 5, 4, 6, 8, 9];
 	assert(mergeSort(input) == [0, 4, 5, 6, 7, 8, 9]);
@@ -104,7 +104,7 @@ unittest{
 }
 
 /// Merge 2 sorted arrays.
-/// 
+///
 /// if maxLen is non zero, only first maxLen number of elements are returned.
 /// using maxLen should give slight performance benefit
 T[] merge(alias val = "a", T)(T[] A, T[] B, size_t maxLen = 0){
